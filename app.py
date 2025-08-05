54517d9ff796e8f320af7bbad886662696a1dacf
tonik007 <uutonikbms77453@gmail.com>
2025-08-05 16:02:13 +0600

    final commit

--- app.py
+++ app.py
@@ -62,7 +62,8 @@ def index():
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
+
